928579a58cb46399f0d92151fe833bfc8162ff6d
Sebastian OM <sebastianobandomorales@gmail.com>
2024-07-16 08:15:10 -0500

    Update problem_6.py

--- p2/problem_6.py
+++ p2/problem_6.py
@@ -4,13 +4,26 @@ def get_min_max(ints):
     
     Args:
        ints(list): list of integers containing one or more integers
+    Returns:
+       tuple: (min, max)
     """
-   pass
+    if not ints:
+        return None, None
+    
+    min_val = max_val = ints[0]
+    
+    for num in ints[1:]:
+        if num < min_val:
+            min_val = num
+        if num > max_val:
+            max_val = num
+    
+    return min_val, max_val
 
-### Example Test Case of Ten Integers
+# Example Test Case of Ten Integers
 import random
 
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
 
-print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+print("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
