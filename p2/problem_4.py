1af426213099962f79f39aa051d098c99c2132ab
Sebastian OM <sebastianobandomorales@gmail.com>
2024-07-16 08:12:20 -0500

    Update problem_4.py

--- p2/problem_4.py
+++ p2/problem_4.py
@@ -1,11 +1,28 @@
 def sort_012(input_list):
     """
-    Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal.
+    Given an input array consisting of only 0, 1, and 2, sort the array in a single traversal.
 
     Args:
        input_list(list): List to be sorted
+    Returns:
+       list: Sorted list
     """
-    pass
+    low = 0
+    mid = 0
+    high = len(input_list) - 1
+    
+    while mid <= high:
+        if input_list[mid] == 0:
+            input_list[low], input_list[mid] = input_list[mid], input_list[low]
+            low += 1
+            mid += 1
+        elif input_list[mid] == 1:
+            mid += 1
+        else:
+            input_list[mid], input_list[high] = input_list[high], input_list[mid]
+            high -= 1
+    
+    return input_list
 
 def test_function(test_case):
     sorted_array = sort_012(test_case)
